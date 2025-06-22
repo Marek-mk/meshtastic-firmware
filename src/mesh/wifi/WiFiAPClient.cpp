b2ba081fc5a78907561205de6b64e77abe3ff86e
Marek-mk <118679709+Marek-mk@users.noreply.github.com>
2025-06-22 23:20:16 +0200

    heltec v.2.1 @80MHz test, with brownout

--- src/mesh/wifi/WiFiAPClient.cpp
+++ src/mesh/wifi/WiFiAPClient.cpp
@@ -283,7 +283,7 @@ bool initWifi()
 
             WiFi.mode(WIFI_STA);
             WiFi.setHostname(ourHost);
-            WiFi.setTxPower(WIFI_POWER_13dBm); // mx
+            WiFi.setTxPower(WIFI_POWER_11dBm); // mx
 
             if (config.network.address_mode == meshtastic_Config_NetworkConfig_AddressMode_STATIC &&
                 config.network.ipv4_config.ip != 0) {
@@ -298,7 +298,7 @@ bool initWifi()
 #ifdef ARCH_ESP32
             WiFi.onEvent(WiFiEvent);
             WiFi.setAutoReconnect(true);
-            WiFi.setTxPower(WIFI_POWER_13dBm); // mx
+            WiFi.setTxPower(WIFI_POWER_11dBm); // mx
             // WiFi.setSleep(false);
             WiFi.setSleep(WIFI_PS_MIN_MODEM); // mx
             // WiFi.setSleep(WIFI_PS_NONE);
